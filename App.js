0385c3c026fde2adcaf9e955407090fef5194e01
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2021-02-18 16:15:59 +0900

    Language update

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: "Search",
     defaultNavigationOptions: {
-      title: "Yemoq Qidirish",
+      title: "Boqivoy Foods",
     },
   }
 );
